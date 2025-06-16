ce60fcae15d6bde05618c27b4f21289e742fc556
Ardian Sallauka <ardi_c_cc@hotmail.com>
2025-06-16 16:03:38 +0200

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'v0.dev',,
+  // add favicon
+  icons: {
+    icon: '/images/GTA_logo.png'
 }
 
 export default function RootLayout({
